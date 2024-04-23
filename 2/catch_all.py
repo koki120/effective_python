b59d73ccde0ccb187daca9d35b124090f5d22060
nakatsu <k-nakatsu@digeon.co>
2024-04-23 16:16:42 +0000

    fix comment

--- 2/catch_all.py
+++ 2/catch_all.py
@@ -10,17 +10,18 @@ b = a[:2]
 first, second, *other = a
 assert [first, second] == b
 
-# catch-allアンパックは*を複数とることができない。構文エラーを起こす。
+
+# catch-allアンパックは*を複数とることができない。構文エラーを起こす。以下がその例である。
 # first, *middle, *second_middle, last = [1, 2, 3, 4, 5]
 
-# 指定部は少なくとも一つは必要。
+
+# 指定部は少なくとも一つは必要。以下がその例である。
 # *all = [1, 2]
 
-# アスタリスク付きの式は常にlist型を返すので、メモリを使い切らないように注意を払う必要がある。
 
+# アスタリスク付きの式は常にlist型を返すので、メモリを使い切らないように注意を払う必要がある。
 generator = ((i, hash(str(i))) for i in range(1000))
 
-# firstはタプル、otherはリスト
-first, *other = generator
+first, *other = generator  # firstはタプル、otherはリスト
 
 assert len(other) == 999
--- 2/sort.py
+++ 2/sort.py
@@ -2,8 +2,7 @@
 ソートについて
 """
 
-# 組み込み型listのsortメソッドはほとんどの組み込み型で提供されている。
-
+# listのsortメソッドはほとんどの組み込み型で提供されている。
 numbers = [324, 45423, 563, 21, 45, 534643]
 numbers.sort()
 assert numbers == [21, 45, 324, 563, 45423, 534643]
@@ -18,7 +17,7 @@ tuples.sort()
 assert tuples == [(23, "🗻"), (123, "🍖", "🐄"), (123, "🐡")]
 
 
-# classは比較を定義しないとsortを呼び出すことはできない。
+# classは比較を定義しないとsortを呼び出すことはできない。以下のtoolsに対して、sortを呼び出すとエラーになる。
 class Tool:
     def __init__(self, name, weight) -> None:
         self.name = name
@@ -27,8 +26,6 @@ class Tool:
     def __repr__(self) -> str:
         return f"Tool({self.name}, {self.weight})"
 
-
-# tools.sort()は呼び出せない。
 tools = [
     Tool("level", 3.5),
     Tool("hammer", 1.55),
@@ -38,12 +35,12 @@ tools = [
 
 
 # クラスをソートしたい場合は、keyパラメータを使用する。key関数の第一引数はリストの要素が渡され、戻り値は比較可能な値である必要がある。
-tools.sort(key=lambda x: (-x.weight, x.name))
-# [Tool(level, 3.5), Tool(hammer, 1.55), Tool(screwdriver, 0.5), Tool(chisel, 0.25)]
+tools.sort(
+    key=lambda x: (-x.weight, x.name)
+)  # [Tool(level, 3.5), Tool(hammer, 1.55), Tool(screwdriver, 0.5), Tool(chisel, 0.25)]
 
-# pythonのソートは安定ソートアルゴリズムが使われているので、同じ値の場合は順番は変わらない。
-# この性質を利用して、sort(key=lambda x:(x.weight, -x.name))を下のように実現できる。-x.nameは使えないので、注意。
-
+# TIPS: pythonのソートは安定ソートアルゴリズムが使われているので、同じ値の場合は順番は変わらない。
+# この性質を利用して、sort(key=lambda x:(x.weight, -x.name))を下のように実現できる。文字列（x.name）にマイナス演算子(-)は使えないので、注意。
 
 tools2 = [
     Tool("circular saw", 5),
@@ -53,5 +50,6 @@ tools2 = [
 ]
 
 tools2.sort(key=lambda x: x.name, reverse=True)
-tools2.sort(key=lambda x: x.weight)
-# [Tool(sander, 4), Tool(drill, 4), Tool(circular saw, 5), Tool(jackhammer, 40)]
+tools2.sort(
+    key=lambda x: x.weight
+)  # [Tool(sander, 4), Tool(drill, 4), Tool(circular saw, 5), Tool(jackhammer, 40)]
